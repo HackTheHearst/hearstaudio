de607b4dad666be170983f1d7caf82adc9c71110
jltran <guardian@berkeley.edu>
2014-09-20 18:39:17 -0700

    used Flask interface

---
+++ hello.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+import requests
+import json
+from jinja2 import Environment, PackageLoader
+
+app = Flask(__name__)
+
+@app.route('/')
+def input():
+	env = Environment(loader=PackageLoader('scriptname', 'template'))
+	template = env.get_template('input.html')
+	return template.render()
+
+@app.route('/output', methods=['POST'])
+def output():
+    #Headers with API Keys
+	url = 'https://apis-qa.berkeley.edu/hearst_museum/select'
+	headers = {}
+	headers['app_key'] = "43ca2baad599b4ffed25c2fc22c4dbfd"
+	headers['app_id'] = "20a6d157"
+
+	#Defines parameters
+	params = {
+		'q': 'objname_s:fork',
+		#'q': 'objcollector_ss: Dr. David P. Barrows',
+		'wt': 'json',
+		'indent': 'True',
+		'facet': 'true',
+		'facet.field':'blob_ss',
+		'rows': 0
+	}
+	r = requests.get(url, params=params, headers=headers)		
+
+	#array of images we push to the website
+	imgArr = []
+	#set an limit to the number of itmes we get
+	itemLimit = 20
+	data = json.loads(r.text)
+	print data['response']['numFound']
+	for item in data['facet_counts']['facet_fields']['blob_ss']:
+		if item > 100 and itemLimit > 0:
+			imgArr.append(item)
+			itemLimit -= 1
+
+	env = Environment(loader=PackageLoader('scriptname', 'template'))
+	template = env.get_template('myhtmlpage.html')
+	templateVars = {
+		"title":		"Test Example",
+		"description":	"A simple inquiry of function.",
+		"array": imgArr
+	}
+
+	return template.render(templateVars)
+
+if __name__ == '__main__':
+    app.run()
